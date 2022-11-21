322aaf63db027214b13b21a8704ba8dee2e6d39d
zahra shakeri <32194844+zshakerii@users.noreply.github.com>
2022-11-21 11:11:31 +0330

    1401.08.30-start

---
+++ OnlineChat.PersistantMain/Chats/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineChat.PersistantMain.Chats
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public int Reciver { get; set; }
+        public int Sender { get; set; }
+    }
+}
